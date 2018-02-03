8c86c5e7a96576a73df1768fda2a0e1077fc7b1d
Jim Tryon <tryonjf@gmail.com>
2018-02-03 02:27:23 -0800

    Add entry point for hello world text

---
+++ SpringBoot/src/main/java/HelloController.java
@@ -0,0 +1,12 @@
+package com.ugrow.SpringBoot;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+}
