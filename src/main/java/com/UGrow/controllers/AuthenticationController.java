96680925c177da84783390a4a10bb82ac3e00459
Wilson Chan <Wilson_chan789@yahoo.com>
2018-02-05 03:15:04 -0800

    "Finish the backend for login and register, need connection to database and controller for home page and templates for login/register page and home page"

---
+++ src/main/java/com/UGrow/controllers/AuthenticationController.java
@@ -0,0 +1,106 @@
+package com.UGrow.controllers;
+
+import com.UGrow.models.User;
+import com.UGrow.models.data.UserDao;
+import com.UGrow.models.forms.LoginForm;
+import com.UGrow.models.forms.RegisterForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+@Controller
+public class AuthenticationController extends AbstractController {
+
+    @Autowired
+    protected UserDao userDao;
+
+    @RequestMapping(value = "register", method = RequestMethod.GET)
+    public String displayRegister(HttpServletRequest request, Model model) {
+        model.addAttribute("sessionOn", isSessionActive(request.getSession()));
+        model.addAttribute("title", "Register");
+        model.addAttribute("registerForm", new RegisterForm());
+        return "user/register";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public String displayLogin(Model model, HttpServletRequest request) {
+        model.addAttribute("sessionOn", isSessionActive(request.getSession()));
+        model.addAttribute("title", "Login");
+        model.addAttribute("loginForm", new LoginForm());
+        return "user/login";
+    }
+
+    @RequestMapping(value = "register", method = RequestMethod.POST)
+    public String register(Model model, @ModelAttribute @Valid RegisterForm registerForm, Errors errors, HttpServletRequest request) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Register");
+            model.addAttribute("sessionOn", isSessionActive(request.getSession()));
+            return "user/register";
+        }
+
+        User existUser = userDao.findByUsername(registerForm.getUsername());
+
+        if (existUser != null) {
+            model.addAttribute("existingUsername", "Username already exists");
+            model.addAttribute("title", "Register");
+            model.addAttribute("sessionOn", isSessionActive(request.getSession()));
+            return "user/register";
+        }
+        String verifyError = "";
+        if (registerForm.getVerifyPassword().equals(registerForm.getPassword())) {
+            User newUser = new User(registerForm.getUsername(), registerForm.getPassword());
+            userDao.save(newUser);
+            setUserInSession(request.getSession(), newUser);
+            return "redirect:/home";
+        } else {
+            verifyError = "Please enter a matching Password";
+            model.addAttribute("title", "Register");
+            model.addAttribute("verifyError", verifyError);
+            model.addAttribute("sessionOn", isSessionActive(request.getSession()));
+            return "user/register";
+        }
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String login(Model model, @ModelAttribute @Valid LoginForm loginForm, Errors errors, HttpServletRequest request) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Login");
+            model.addAttribute("sessionOn", isSessionActive(request.getSession()));
+            return "user/login";
+        }
+
+        User user = userDao.findByUsername(loginForm.getUsername());
+        String password = loginForm.getPassword();
+
+        if (user == null) {
+            model.addAttribute("usernameError", "Invalid username! Please try again!");
+            model.addAttribute("sessionOn", isSessionActive(request.getSession()));
+            model.addAttribute("title", "Login");
+            return "user/login";
+        }
+
+        if (!user.isMatchingPassword(password)) {
+            model.addAttribute("passwordError", "Wrong password! Please try again!");
+            model.addAttribute("sessionOn", isSessionActive(request.getSession()));
+            model.addAttribute("title", "Login");
+            return "user/login";
+        }
+
+        setUserInSession(request.getSession(), user);
+        return "redirect:/home";
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logout(HttpServletRequest request){
+        request.getSession().invalidate();
+        return "redirect:/login";
+    }
+}
