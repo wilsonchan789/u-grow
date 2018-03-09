1f4f6ebf1cca3ccbc5be0a702f78529397d02f66
Wilson Chan <Wilson_chan789@yahoo.com>
2018-03-08 20:01:45 -0800

    Add todo list into the home page but need to change it to be able to click an option and add into calendar and display todo

--- src/main/java/com/UGrow/controllers/HomeController.java
+++ src/main/java/com/UGrow/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package com.UGrow.controllers;
 
+import com.UGrow.models.Todo;
 import com.UGrow.models.User;
+import com.UGrow.models.data.TodoDao;
 import com.UGrow.models.data.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,16 +17,19 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class HomeController extends AbstractController {
 
+    @Autowired
+    private TodoDao todoDao;
+
     @RequestMapping(value = "/")
     public RedirectView index(Model model, HttpServletRequest request) {
-
         model.addAttribute("title", "U-Grow");
         model.addAttribute("sessionOn", isSessionActive(request.getSession()));
         return new RedirectView("home");
     }
     @RequestMapping(value= "/home")
-    public String displayHome(Model model, HttpServletRequest request) {
-
+    public String displayHome(Model model, int id, HttpServletRequest request) {
+        Todo todo = todoDao.findOne(id);
+        model.addAttribute("todo", todo);
         User user = getUserFromSession(request.getSession());
         String username =  user.getUsername();
         model.addAttribute("username", username);
